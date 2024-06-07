a51b952e9223296205e52077932009ea8d7e7366
h-hyakut <hocha01n@gmail.com>
2024-06-07 16:50:02 +0900

    07-2

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,5 @@
 class ApplicationController < ActionController::Base
+  before_action :configure_permitted_parameters, if: :devise_controller?
   def after_sign_in_path_for(resources)
     mypage_path
   end
@@ -7,3 +8,8 @@ class ApplicationController < ActionController::Base
     new_user_session_path
   end
 end
+
+private
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :email, :password, :password_confirmation])
+  end
